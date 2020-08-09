06b14a2d3afa9e411991466b0e373ed5268b6ca7
contrapunctus <contrapunctus@disroot.org>
2020-08-09 19:21:23 +0530

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((dired-mode . ((dired-omit-mode . t)
+                (dired-omit-extensions . (".html")))))
